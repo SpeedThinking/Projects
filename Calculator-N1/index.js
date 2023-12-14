bef44a7c7b54cd183b13a430e7cd47323afd825f
Pablo Manjarres Negrette <andresmanjarresneg@gmail.com>
2023-12-14 05:39:37 -0500

    calculator works now!

--- Calculator-N1/index.js
+++ Calculator-N1/index.js
@@ -5,6 +5,8 @@ display.indexHTML = "0";
 
 
 function setUp() {
+  randomTitleColor();
+
   for (let button of buttons) {
     button.addEventListener("click", (e) => {
     if(e.target.getElementsByClassName('keypad-button')) {
@@ -16,36 +18,103 @@ function setUp() {
   }
 
 }
+function randomTitleColor() {
+  function changeColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const title = document.getElementById("title");
+    title.style.color = "#" + randomColor;
+  }
+
+  // Call the changeColor function every 1 second (1000 milliseconds)
+  setInterval(changeColor, 1000);
+}
 
 function handleButtonClick(button) {
   const buttonText = button.innerHTML;
 
-  if(buttonText === 'C' || buttonText === 'CE') {
+  if (buttonText === 'C' || buttonText === 'CE') {
     clearScreen();
-  } else if(buttonText === '←') {
+  } else if (buttonText === '←') {
     removeLastCharacter();
-  } else if(buttonText === '=') {
-    calculate();
+  } else if (buttonText === '=') {
+    calculate(display);
+  } else if (buttonText === '-') {
+    handleNegative();
+  } else if (buttonText === '√') {
+    calculateRoot(2); // Square root
+  } else if (buttonText === '∛') {
+    calculateRoot(3); // Cube root
   } else {
     updateScreen(buttonText);
   }
 }
 
+function calculateRoot(root) {
+  const currentDisplay = parseFloat(display.innerHTML);
+  if (!isNaN(currentDisplay)) {
+    const result = Math.pow(currentDisplay, 1 / root);
+    display.innerHTML = result;
+  } else {
+    display.innerHTML = 'Error';
+  }
+}
+
+function handleNegative() {
+  const currentDisplay = display.innerHTML;
+  if (currentDisplay === '0') {
+    display.innerHTML = '-';
+  } else if (currentDisplay.includes('-')) {
+    display.innerHTML = `-${currentDisplay}`;
+  } else {
+    display.innerHTML = currentDisplay.substring(1); // Remove the existing '-'
+  }
+}
 
 
+function calculate(display) {
+  try {
+    let displayActual = display.innerHTML;
+    let result = evaluateExpression(displayActual);
+    if (result === undefined || isNaN(result)) {
+      display.innerHTML = 'Error';
+    } else if (!isFinite(result)) {
+      display.innerHTML = 'Cannot divide by zero';
+    } else {
+      display.innerHTML = result;
+    }
+    return result;
+  } catch (error) {
+    display.innerHTML = wrongInput(display.innerHTML, '=');
+  }
+}
+
+
+function evaluateExpression(expression) {
+  const sanitizedExpression = expression.replace(/÷/g, '/').replace(/x/g, '*').replace(/−/g, '-').replace(/√x/g, 'Math.sqrt').replace(/%/g, '/100')
+  return Function(`'use strict'; return (${sanitizedExpression})`)();
+}
 function clearScreen() {
   display.innerHTML = "0";
 }
 
 function removeLastCharacter() {
-  display.innerHTML = display.innerHTML.slice(0, -1);
+  if(display.innerHTML === '0') {
+    return
+  } else if(display.innerHTML.length === 1) {
+    display.innerHTML = display.innerHTML.slice(0, -1);
+    display.innerHTML = '0';
+  } else {
+    display.innerHTML = display.innerHTML.slice(0, -1);
+  }
 }
 
 function updateScreen(text) {
-  if (display.innerHTML === '0') {
+  if (text === '.') {
+    if (!display.innerHTML.includes('.')) {
+      display.innerHTML += text;
+    }
+  } else if (display.innerHTML === '0') {
     display.innerHTML = text;
-  } else if (text === '.' && !display.innerHTML.endsWith('0')) { // Problems with the zero and point too
-    display.innerHTML += '0' + text;
   } else {
     display.innerHTML += text;
   }
@@ -53,7 +122,7 @@ function updateScreen(text) {
 
 
 function wrongInput(number, symbol) {
-  return `Wrong input: ${number} ${symbol} ${display.innerHTML}`
+  return `Wrong input: ${display.innerHTML} ${SyntaxError}`
 }
 
 window.addEventListener('load', setUp, false);  // Starts the script
